0852efdff73f28191f1ecaa4054d2dda0ef047ba
Steven <christopher.torres@catolica.edu.sv>
2026-08-12 17:54:04 -0600

    reto08 - generador de tablas con for

---
+++ Reto08_GeneradorTablas/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("ingresa el numero: ");
+int numero = int.Parse(Console.ReadLine()!);
+
+Console.Write("ingresa el inicio del rango: ");
+int inicio = int.Parse(Console.ReadLine()!);
+
+Console.Write("ingresa el fin del rango: ");
+int fin = int.Parse(Console.ReadLine()!);
+
+if (inicio > fin)
+{
+    Console.WriteLine("el inicio no puede ser mayor que el fin.");
+}
+else
+{
+    Console.WriteLine($"\ntabla del {numero} del {inicio} al {fin}:\n");
+
+    for (int i = inicio; i <= fin; i++)
+    {
+        int resultado = numero * i;
+        Console.WriteLine($"{numero} x {i} = {resultado}");
+    }
+}
