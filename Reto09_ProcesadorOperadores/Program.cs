e61e4a96d5cf8ef6f7e87165841049e6fe4dc570
Steven <christopher.torres@catolica.edu.sv>
2026-08-12 17:58:12 -0600

    reto09 - procesador de operadores con foreach e if

---
+++ Reto09_ProcesadorOperadores/Program.cs
@@ -0,0 +1,11 @@
+﻿string[] nombres = { "Ana", "Carlos", "Luis", "Gabriela", "Eva", "Roberto", "Kim", "Alejandro" };
+
+Console.WriteLine("nombres con mas de 4 caracteres:\n");
+
+foreach (string nombre in nombres)
+{
+    if (nombre.Length > 4)
+    {
+        Console.WriteLine(nombre);
+    }
+}
