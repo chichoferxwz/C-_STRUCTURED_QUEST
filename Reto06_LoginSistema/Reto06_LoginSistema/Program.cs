57a5e1faf7f1c0f6aa31d167d238948913f74971
Steven <christopher.torres@catolica.edu.sv>
2026-08-12 17:38:41 -0600

    reto06 - login del sistema con while y 3 intentos

---
+++ Reto06_LoginSistema/Reto06_LoginSistema/Program.cs
@@ -0,0 +1,29 @@
+﻿
+string usuarioCorrecto = "admin";
+string contraseñaCorrecta = "1234";
+
+int intentos = 3;
+
+while (intentos > 0)
+{
+    Console.Write("usuario: ");
+    string usuario = Console.ReadLine()!;
+
+    Console.Write("contrasena: ");
+    string contrasena = Console.ReadLine()!;
+
+    if (usuario == usuarioCorrecto && contrasena == contraseñaCorrecta)
+    {
+        Console.WriteLine("acceso concedido. bienvenido al sistema.");
+        break;
+    }
+    else
+    {
+        intentos--;  // le quita un intento a usuario
+        if (intentos > 0)
+            Console.WriteLine($"credenciales incorrectas. intentos restantes: {intentos}");
+    }
+}
+
+if (intentos == 0)
+    Console.WriteLine("SISTEMA BLOQUEADO");
